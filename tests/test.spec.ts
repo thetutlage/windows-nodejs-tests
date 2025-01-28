8fa54f637638b6128061e61bdbd2803037939211
Harminder Virk <virk.officials@gmail.com>
2025-01-28 21:27:00 +0530

    add linting setup

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -3,20 +3,24 @@ import test from 'node:test'
 import { ErrorsPrinter } from '@japa/errors-printer'
 import { sum } from '../index.js'
 
-test.describe('setup', () => {
-  test('sum two numbers', async () => {
+test.describe('Errors printer', () => {
+  test('print assert error', async () => {
     try {
       assert.equal(sum(2, 2), 5)
     } catch (error) {
       await new ErrorsPrinter().printError(error)
+      console.log('PARSED')
+      console.log(await new ErrorsPrinter().parseError(error))
     }
   })
 
-  test('another failing test', async () => {
+  test('print assert error', async () => {
     try {
       assert.equal(sum(2, 2), 5)
     } catch (error) {
       await new ErrorsPrinter().printError(error)
+      console.log('PARSED')
+      console.log(await new ErrorsPrinter().parseError(error))
     }
   })
 })
