7044b67788dc84b9baf46598609e61f360c99ca7
Harminder Virk <virk.officials@gmail.com>
2025-01-28 23:12:29 +0530

    wip

--- src/index.ts
+++ src/index.ts
@@ -14,58 +14,6 @@
  * tests executed via Japa.
  */
 export class ErrorsPrinter {
-  // #options: {
-  //   stackLinesCount: number
-  //   framesMaxLimit: number
-  // }
-
-  // constructor(options?: { stackLinesCount?: number; framesMaxLimit?: number }) {
-  //   this.#options = { stackLinesCount: 5, framesMaxLimit: 3, ...options }
-  // }
-
-  /**
-   * Returns human readable message for error phase
-   */
-  // #getPhaseTitle(phase: string) {
-  //   switch (phase) {
-  //     case 'setup':
-  //       return 'Setup hook'
-  //     case 'setup:cleanup':
-  //       return 'Setup hook cleanup function'
-  //     case 'teardown':
-  //       return 'Teardown hook'
-  //     case 'teardown:cleanup':
-  //       return 'Teardown hook cleanup function'
-  //   }
-  // }
-
-  /**
-   * Parses the error stack using Youch
-   */
-  // #parseErrorStack(error: any) {
-  //   return error
-  //   // return new Youch().toJSON(error, {
-  //   //   frameSourceBuffer: this.#options.stackLinesCount,
-  //   // })
-  // }
-
-  /**
-   * Parsers chai assertion error
-   */
-  // async parseAssertionError(_: any) {
-  //   // const parsedError = error
-  //   // if (error.showDiff) {
-  //   //   console.error()
-  //   //   // const { actual, expected } = error
-  //   //   // const diff = jestDiff(expected, actual, {
-  //   //   //   expand: true,
-  //   //   //   includeChangeCounts: true,
-  //   //   // })
-  //   //   // parsedError.message = `${parsedError.message}\n${diff}`
-  //   // }
-  //   // return parsedError
-  // }
-
   /**
    * Displays the error stack for a given error
    */
