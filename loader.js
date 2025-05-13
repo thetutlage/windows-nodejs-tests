49c35b384438ab5e161de44bf76550b88722e4a3
Harminder Virk <virk.officials@gmail.com>
2025-05-13 14:23:02 +0530

    refactor: compile using swc

--- loader.js
+++ loader.js
@@ -1,7 +1,29 @@
+import { transformSync } from '@swc/core'
+
 export async function initialize() {}
 export async function resolve(specifier, context, nextResolve) {
   return nextResolve(specifier, context)
 }
+
 export async function load(url, context, nextLoad) {
-  return nextLoad(url, context)
+  const { source } = await nextLoad(url, context)
+
+  const code = transformSync(source.toString(), {
+    sourceMaps: true,
+    module: {
+      type: 'es6',
+    },
+    jsc: {
+      target: 'esnext',
+      parser: {
+        syntax: 'typescript',
+      },
+    },
+  }).code
+
+  console.log({ code })
+  return {
+    format: 'module',
+    source: code,
+  }
 }
